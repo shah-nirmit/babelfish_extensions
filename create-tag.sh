e2e79488b34932c1ce73ad5d2242d9c0e8f1337c
Shameem Ahmed <shameemahmed20apr2000@gmail.com>
2022-12-23 09:05:55 +0000

    mes corect

--- create-tag.sh
+++ create-tag.sh
@@ -9,7 +9,7 @@ git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 export usage="$(basename "$0") [-h] [-c commit] [-r repo][-m]\n
   -h    help\n
   -c    commit hash; Default commit:HEAD\n
-  -t    tag name
+  -t    tag name\n
   -m    message"
 
 
