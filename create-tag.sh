1bf50c6fefbd250cdbbe44e0a62577b898f92ad2
Shameem Ahmed <shameemahmed20apr2000@gmail.com>
2022-12-23 09:01:31 +0000

    message cor

--- create-tag.sh
+++ create-tag.sh
@@ -60,7 +60,7 @@ git remote -vv
 git branch -vv
 
 echo Creating tag $new for commit $commit
-echo ${message}
+echo $message
 git tag -a "${new}" -m "${message}" $commit
 git push origin "${new}"
 # POST a new ref to repo via Github API
