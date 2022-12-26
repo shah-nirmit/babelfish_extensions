2a3931be361e415337a9accc9d9b2060c2fe33a4
Shameem Ahmed <shameemahmed20apr2000@gmail.com>
2022-12-26 11:44:15 +0000

    Manual workflow for creating tags for extension repo

--- create-tag.sh
+++ create-tag.sh
@@ -28,24 +28,20 @@ then
     commit=$(git rev-parse HEAD)
 fi
 
-# if tagname is not provided, fetch the latest tag and increment the patch
-# version by 1.
+# if tagname is not provided, exit
 if [ -z "$new" ]
 then
     echo "Error: Tag not provided!"
     exit 1
 fi
 
-# check the tag format
+# check the tag format (need manual update when necessary)
 format=BABEL_2_2_
 if ! [[ "$new" =~ "$format"[0-9]+ ]]
 then
     echo "Error: Invalid tag prefix, expected: ${format}<number>"
     exit 1
 fi
-
-git remote -vv
-git branch -vv
 
 echo Creating tag $new for commit $commit
 
